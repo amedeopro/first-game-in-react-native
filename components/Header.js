fda8544dc5fac0a7cfeb70a83397bf14c415b139
Amedeo Pro <amedeopro@me.com>
2020-04-25 22:22:10 +0200

    create costants file for colors

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
-
+import Colors from '../costants/colors'
 const Header = props => {
 
     return (
@@ -16,7 +16,7 @@ const styles = StyleSheet.create({
         width: '100%',
         height: 90,
         paddingTop: 36,
-        backgroundColor: '#f7287b',
+        backgroundColor: Colors.primary,
         alignItems: 'center',
         justifyContent: 'center'
     },
