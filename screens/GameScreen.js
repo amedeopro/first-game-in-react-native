56911bda4b66f646aff15daa0bbd772111736335
Amedeo Pro <amedeopro@me.com>
2020-04-27 20:42:42 +0200

    game screen

---
+++ screens/GameScreen.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+const generateRandomBetween = (min, max, exclude) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    const rndNum = Math.floor(Math.random() * (max - min)) + min;
+    if (rndNum === exclude) {
+        return generateRandomBetween(min, max, exclude)
+    } else {
+        return rndNum;
+    }
+}
+
+
+const GameScreen = props => {
+
+    const [currentGUess, setCurrentGuess] = useState(generateRandomBetween(1, 100, props.userChoice))
+
+    return(
+        <View>
+            <Text>Opponent</Text>
+        </View>
+    )
+}
+
+
+const styles = StyleSheet.create({
+
+})
+
+export default GameScreen
